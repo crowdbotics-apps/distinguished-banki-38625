c2c0a553add2aa2bf119dd6b8fe2c251f1a4d59e
Crowdbotics <team@crowdbotics.com>
2023-02-01 08:09:39 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "distinguished-banki-38625",
+  url: "https://distinguished-banki-38625.botics.co",
+  api: "https://distinguished-banki-38625.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
